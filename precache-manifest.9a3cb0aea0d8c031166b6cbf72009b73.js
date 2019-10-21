fae0dd5c234cdaf10a6a77955733f8772c7f6c65
Alex Zhang <>
2019-10-21 10:14:38 -0400

    Updates

---
+++ precache-manifest.9a3cb0aea0d8c031166b6cbf72009b73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add728008c1589e2b71991449991f61c",
+    "url": "/react-b/index.html"
+  },
+  {
+    "revision": "05d3d7cb55f03815e72b",
+    "url": "/react-b/static/css/main.9f029f7b.chunk.css"
+  },
+  {
+    "revision": "bb3069e756a68b5447a1",
+    "url": "/react-b/static/js/2.245abb10.chunk.js"
+  },
+  {
+    "revision": "05d3d7cb55f03815e72b",
+    "url": "/react-b/static/js/main.f6a96bf0.chunk.js"
+  },
+  {
+    "revision": "330ae53b1b20aec8f340",
+    "url": "/react-b/static/js/runtime-main.74f880dd.js"
+  }
+]);
